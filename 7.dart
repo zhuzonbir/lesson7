6f882b8621945e4693cef426b5c98d938bd89c63
zhuzonbir <phnix11.11@gmail.com>
2021-03-26 21:10:25 +0600

    izvinyayus chto tak pozdno

---
+++ 7.dart
@@ -0,0 +1,95 @@
+main() {
+
+
+// var a = [1, 3, 5, 7, 9];
+
+//   int i = a.length-1;
+//   while(i >= 0){
+
+// print(a[i]);
+// --i;
+
+// int n = 9;
+
+// switch (n>9) {
+
+// case true:
+
+// print("Value is 1");
+
+// break;
+
+// case false:
+
+// print("Value is 2");
+
+// break;
+
+// default:
+
+// print("Out of range");
+
+// break;
+
+// }
+// var a = "Nik";
+
+// switch (a) {
+
+// case "Nik":
+
+// print("Molodez eei");
+
+// break;
+
+// case "Das":
+
+// print("NE molodec");
+
+// break;
+
+// default:
+
+// print("nooo");
+
+// break;
+
+// }
+// int nx = 9;
+
+// switch (nx) {
+
+// case  1:
+// print("weqr");
+
+// break;
+
+// case 9:
+
+// print("qwerwfd");
+
+// break;
+
+// default:
+
+// print("nooo");
+
+// break;
+
+// }
+  // stdout.write("");                                   
+  // String manr = stdin.readLineSync()!;
+  // manr.split("");
+  // stdout.write("");
+  // String banr = stdin.readLineSync()!;
+  // banr.split("");
+  // var z = 0;
+  // int i = 0;
+  // while (i <= manr.length - 1) {
+  //   if (manr[i] == banr) {
+  //     z += 1;
+  //   }
+  //   ++i;
+  // }
+  // print(z);
+  }
